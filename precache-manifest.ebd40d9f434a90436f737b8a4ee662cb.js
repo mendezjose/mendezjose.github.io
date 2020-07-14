b8a7be9b47a55cf2368af7634b952e190aeff4b7
mendezjose <42753755+mendezjose@users.noreply.github.com>
2020-07-13 22:04:00 -0500

    Updates

---
+++ precache-manifest.ebd40d9f434a90436f737b8a4ee662cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c31ce522ba563273974e6548b13c9767",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b2813b68b0018d1d784",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67bb25509ee662609b6b",
+    "url": "/static/js/2.0e100a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0e100a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b2813b68b0018d1d784",
+    "url": "/static/js/main.305b76e9.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
