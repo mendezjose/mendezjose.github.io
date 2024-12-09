629198e9a6ba1b233b39b00cb41a466e62718fb9
Jose Mendez <iscjosemendez@gmail.com>
2024-12-09 17:56:33 -0600

    Implement ghost template

--- _next/static/chunks/app/[locale]/page-259eaa9359403a24.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[61],{},function(n){n.O(0,[208,665,240,516,464,581,187,109,102,353,971,117,744],function(){return n(n.s=4223)}),_N_E=n.O()}]);
