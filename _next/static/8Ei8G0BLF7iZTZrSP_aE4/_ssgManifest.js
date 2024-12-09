b1f15f5afa5a286c0d1077f979ad53bd9b4446f4
Jose Mendez <iscjosemendez@gmail.com>
2024-12-09 17:24:30 -0600

    Replace old site with modern site

---
+++ _next/static/8Ei8G0BLF7iZTZrSP_aE4/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fabout","\u002F[locale]\u002Fblog","\u002F[locale]\u002Fblog\u002F[slug]","\u002F[locale]\u002Fgallery","\u002F[locale]\u002Fwork","\u002F[locale]\u002Fwork\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
